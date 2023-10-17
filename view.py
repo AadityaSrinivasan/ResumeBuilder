242957ef4902516c28d38f1f3ab647c16d933a64
SayokBose <sayokbose@gmail.com>
2023-10-17 17:54:55 -0500

    br

--- view.py
+++ view.py
@@ -11,14 +11,10 @@ uploaded_pdf = None
 @view.route("/",  methods = ['GET', "POST"])
 def home():
     global uploaded_pdf
-    print('1')
     if request.method == 'POST':
-        print('2')
         if 'pdf_file' in request.files:
-            print('3')
             pdf_file = request.files['pdf_file']
             if pdf_file.filename != '':
-                print("hi")
                 uploaded_pdf = pdf_file.read()
                 read(uploaded_pdf)
 
