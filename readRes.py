6311f1cc25863c1bd8e46f24e78274a29d083b4f
SayokBose <sayokbose@gmail.com>
2023-11-06 13:43:46 -0600

    updated education

--- readRes.py
+++ readRes.py
@@ -1,6 +1,8 @@
 from pdfminer.high_level import extract_text
 import nltk
-
+from pyresparser import ResumeParser
+from nltk.corpus import stopwords
+import spacy
 import re
 import subprocess
 
@@ -22,7 +24,7 @@ SKILLS_DB = read_skills_from_file(SKILLS_FILE)
 
 
 PHONE_REG = re.compile(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]')
-EMAIL_REG = re.compile(r'[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+')
+EMAIL_REG = re.compile(r'[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+', re.I)
 '''SKILLS_DB = [
     'machine learning',
     'data science',
@@ -31,8 +33,8 @@ EMAIL_REG = re.compile(r'[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+')
     'excel',
     'English',
 ]'''
-
-
+nlp = spacy.load('en_core_web_sm')
+STOPWORDS = set(stopwords.words('english'))
 EDUCATION_WORDS = [
     'school',
     'college',
@@ -50,11 +52,13 @@ EDUCATION_WORDS = [
     'kolej',
     'ünivers',
     'okul',
+    
 ]
 
 
 
 def extract_text_from_pdf(pdf_path):
+    print(type(pdf_path))
     return extract_text(pdf_path)
  
 def extract_names(txt):
@@ -68,6 +72,10 @@ def extract_names(txt):
                 )
  
     return person_names
+
+def extract_namesPP(pdf_path):
+    data = ResumeParser(pdf_path).get_extracted_data()
+    return data.get("name", "Name not found")
 
 def extract_phone_number(resume_text):
     phone = re.findall(PHONE_REG, resume_text)
@@ -110,28 +118,79 @@ def extract_skills(input_text):
  
     return found_skills
 
+def extract_education_name(sentence, keyword):
+    # Use regular expressions to extract the education name
+    pattern = r'\b' + re.escape(keyword) + r'\b(.+)'  # Matches "keyword" followed by the institution name
+    match = re.search(pattern, sentence, re.IGNORECASE)
+    
+    if match:
+        return match.group(1).strip()
+    else:
+        return None
+    
+def extract_educationS(sentence):
+    # Process the input sentence with spaCy
+    doc = nlp(sentence)
+    
+    # Initialize a list to store education-related words
+    education_keywords = ["college", "university", "school"]
+    
+    # Initialize a variable to store the detected education name
+    education_name = None
+    
+    # Iterate through the tokens in the sentence
+    for token in doc:
+        # Check if the token is in the education keywords and is not a stopword
+        if token.text.lower() in education_keywords and token.text.lower() not in STOPWORDS:
+            
+            # Get the text surrounding the keyword to extract the education name
+            education_name = sentence
+            #break  # Stop searching after the first match
+    
+    return education_name
+
+
+def reduceEDU(sentence):
+    # Process the input sentence with spaCy
+    doc = nlp(sentence)
+    
+    # Initialize a variable to store the detected education name
+    education_name = None
+    
+    # Iterate through named entities in the sentence
+    for ent in doc.ents:
+        if ent.label_ == "ORG" or ent.label_ == "EDU":  # Check for organization or education labels
+            education_name = ent.text
+            break  # Stop searching after the first match
+    
+    return education_name
+
 def extract_education(input_text):
     organizations = []
- 
+    education = set()
     # first get all the organization names using nltk
     for sent in nltk.sent_tokenize(input_text):
-        for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
-            if hasattr(chunk, 'label') and chunk.label() == 'ORGANIZATION':
-                organizations.append(' '.join(c[0] for c in chunk.leaves()))
- 
-    # we search for each bigram and trigram for reserved words
-    # (college, university etc...)
-    education = set()
-    for org in organizations:
-        for word in EDUCATION_WORDS:
-            if org.lower().find(word) >= 0:
-                education.add(org)
- 
+        temp = sent.split('\n')
+        line = [item for item in temp if item != ""]
+        for sentence in line:
+            edu = extract_educationS(sentence)
+            
+            if edu is not None:
+                education.add(edu)
+    educations = set()
+    for sentence in education:
+        edu =  reduceEDU(sentence)
+        if edu is not None:
+            educations.add(edu)
     return education
 
-def analyzeRes(text):
+
+
+
+def analyzeRes(text, pdfpath):
     results = {}
-    names = extract_names(text)
+    #names = extract_names(text)
+    names = extract_namesPP(pdfpath)
     number = extract_phone_number(text)
     email= extract_emails(text)
     skills = extract_skills(text)
@@ -141,7 +200,7 @@ def analyzeRes(text):
 
 
     if names:
-        results['names'] = names[0]
+        results['names'] = names
     if number:
         results['number'] = number
     if email:
@@ -152,7 +211,8 @@ def analyzeRes(text):
     if education:
         results['education'] = education
 
-
+    for key, value in results.items():
+        print(f"{key}: {value}")
 
     return results
 
