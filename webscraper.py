98738f198f3aad2b49d36da4f7edd24754c820f5
SayokBose <sayokbose@gmail.com>
2024-01-25 16:49:56 -0600

    ,

--- webscraper.py
+++ webscraper.py
@@ -1,22 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 from bs4.element import Tag
+from readRes import *
 
-blocks = ["p", "h1", "h2", "h3", "h4", "h5", "blockquote", "ul"]
+def get_skills_from_website(url):
+    blocks = ["p", "h1", "h2", "h3", "h4", "h5", "blockquote", "ul"]
 
-url = "https://jobs.jobvite.com/xylem/job/oJEMqfwj?nl=1&nl=1&fr=false&ref=Simplify"
-
-def getTextFromWebstie(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+    rawTxt = str(soup)
+    skills = extract_skills(rawTxt)
 
-    output = ''
-    for element in soup.find_all(blocks):
-        if isinstance(element, Tag):
-            if element.name == 'ul':
-                for li in element.find_all('li'):
-                    output += '{} '.format(li.get_text(strip=True))
-            else:
-                output += '{} '.format(element.get_text(strip=True))
 
-print(getTextFromWebstie(url))
+    return skills
+
+
